d2d3ab1b74e217d02f22696fefdf9cbf8c15f0c5
Kate <klynn726@yahoo.com>
2020-12-22 18:18:43 -0700

    basic js structure

---
+++ assets/js/game.js
@@ -0,0 +1,9 @@
+var playerName = window.prompt("What is your robot's name?");
+// what is the name
+console.log("Our robot's name is " + playerName);
+
+// fight function
+function fight () {
+  window.alert("The fight has begun!");
+}
+// fight ();
