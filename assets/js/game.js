bf785c3ec81d03a0d4c28b01c7546229ce22033c
Kate <klynn726@yahoo.com>
2020-12-27 17:21:14 -0700

    bug fix null name

--- assets/js/game.js
+++ assets/js/game.js
@@ -11,6 +11,7 @@ var startGame = function() {
     if (playerInfo.health > 0) {
       // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
       window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
+      
 
       // pick new enemy to fight based on the index of the enemyInfo array
       var pickedEnemyObj = enemyInfo[i];
@@ -164,6 +165,18 @@ var shop = function() {
       break;
   }
 };
+//function to get a player name
+
+// function to set name
+var getPlayerName = function() {
+  var name = "";
+
+  while (name === "" || name === null) {
+    name = prompt("What is your robot's name?");
+  }
+  console.log("Your robot's name is ", "", + name);
+  return name;
+};
 
 // function to generate a random numeric value
 var randomNumber = function(min, max) {
@@ -177,7 +190,7 @@ var randomNumber = function(min, max) {
 
 // player information
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
